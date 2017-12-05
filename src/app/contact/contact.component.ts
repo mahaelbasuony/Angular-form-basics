d97368d4f2b4d654a46fe192f11407927cbcb447
Maha Elbasuony <mahaelbasuony@gmail.com>
2017-12-05 13:50:43 +0100

    Added radio buttons ,select, options and date

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  languages=['English','Dutch','Other'];
   constructor() { }
 
   ngOnInit() {
